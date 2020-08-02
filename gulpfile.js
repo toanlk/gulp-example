daa064a2133ad61cabeed000935567d1dc8c0189
Toan Le Khanh <toanlk@Kali.local>
2020-08-02 23:18:26 +0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { series, parallel } = require('gulp');
+
+function clean(cb) {
+  // body omitted
+  cb();
+}
+
+function css(cb) {
+  // body omitted
+  cb();
+}
+
+function javascript(cb) {
+  // body omitted
+  cb();
+}
+
+// exports.build = series(clean, parallel(css, javascript));
+exports.default = series(clean, parallel(css, javascript));
